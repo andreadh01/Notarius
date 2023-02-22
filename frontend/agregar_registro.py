2439feec46b7fb53c590dd3ec4b7be111f20b1c3
Karolina Badilla <karolina14badilla@gmail.com>
2023-02-22 01:34:18 -0700

    interfaz de agregar registro

---
+++ frontend/agregar_registro.py
@@ -0,0 +1,405 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'Inputs.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QGraphicsDropShadowEffect
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(750, 539)
+        MainWindow.setStyleSheet("QMainWindow {\n"
+"background-color:white;\n"
+"\n"
+"}")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setStyleSheet("")
+        self.centralwidget.setObjectName("centralwidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.verticalLayout_5 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_5.setObjectName("verticalLayout_5")
+        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_5.addItem(spacerItem)
+        spacerItem1 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_5.addItem(spacerItem1)
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setStyleSheet("font: 26pt \"Arial Black\";\n"
+"color: rgb(149, 117, 61);")
+        self.label_2.setObjectName("label_2")
+        self.verticalLayout_5.addWidget(self.label_2)
+        spacerItem2 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_5.addItem(spacerItem2)
+        spacerItem3 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_5.addItem(spacerItem3)
+        spacerItem4 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_5.addItem(spacerItem4)
+        self.verticalLayout_6 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_6.setObjectName("verticalLayout_6")
+        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_4.setStyleSheet("QPushButton {\n"
+"    color: rgb(141, 110, 58);\n"
+"    border: none;\n"
+"    font: 10pt \"Arial Rounded MT Bold\";\n"
+"    height: 25px;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"    background-color: rgb(149, 117, 61);\n"
+"    color: rgb(255, 255, 255);\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    border-style: inset;\n"
+"    background-color: rgb(149, 117, 61);\n"
+"    color: rgb(255, 255, 255);\n"
+"}\n"
+"")
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("bar-chart-box-line.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton_4.setIcon(icon)
+        self.pushButton_4.setAutoDefault(False)
+        self.pushButton_4.setFlat(False)
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.verticalLayout_6.addWidget(self.pushButton_4)
+        self.pushButton_6 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_6.setStyleSheet("QPushButton {\n"
+"    color: rgb(141, 110, 58);\n"
+"    border: none;\n"
+"    font: 10pt \"Arial Rounded MT Bold\";\n"
+"    height: 25px;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"    background-color: rgb(149, 117, 61);\n"
+"    color: rgb(255, 255, 255);\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    border-style: inset;\n"
+"    background-color: rgb(149, 117, 61);\n"
+"    color: rgb(255, 255, 255);\n"
+"}")
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap("file-history-fill.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton_6.setIcon(icon1)
+        self.pushButton_6.setObjectName("pushButton_6")
+        self.verticalLayout_6.addWidget(self.pushButton_6)
+        self.pushButton_9 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_9.setStyleSheet("QPushButton {\n"
+"    color: rgb(141, 110, 58);\n"
+"    border: none;\n"
+"    font: 10pt \"Arial Rounded MT Bold\";\n"
+"    height: 25px;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"    background-color: rgb(149, 117, 61);\n"
+"    color: rgb(255, 255, 255);\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    border-style: inset;\n"
+"    background-color: rgb(149, 117, 61);\n"
+"    color: rgb(255, 255, 255);\n"
+"}")
+        icon2 = QtGui.QIcon()
+        icon2.addPixmap(QtGui.QPixmap("file-excel-2-fill.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton_9.setIcon(icon2)
+        self.pushButton_9.setObjectName("pushButton_9")
+        self.verticalLayout_6.addWidget(self.pushButton_9)
+        self.pushButton_8 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_8.setStyleSheet("QPushButton {\n"
+"    color: rgb(141, 110, 58);\n"
+"    border: none;\n"
+"    font: 10pt \"Arial Rounded MT Bold\";\n"
+"height: 25px;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"    background-color: rgb(149, 117, 61);\n"
+"    color: rgb(255, 255, 255);\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    border-style: inset;\n"
+"    background-color: rgb(149, 117, 61);\n"
+"    color: rgb(255, 255, 255);\n"
+"}")
+        icon3 = QtGui.QIcon()
+        icon3.addPixmap(QtGui.QPixmap("profile-line.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton_8.setIcon(icon3)
+        self.pushButton_8.setObjectName("pushButton_8")
+        self.verticalLayout_6.addWidget(self.pushButton_8)
+        self.pushButton_7 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_7.setStyleSheet("QPushButton {\n"
+"    color: rgb(141, 110, 58);\n"
+"    border: none;\n"
+"    font: 10pt \"Arial Rounded MT Bold\";\n"
+"height: 25px;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"    background-color: rgb(149, 117, 61);\n"
+"    color: rgb(255, 255, 255);\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    border-style: inset;\n"
+"    background-color: rgb(149, 117, 61);\n"
+"    color: rgb(255, 255, 255);\n"
+"}")
+        icon4 = QtGui.QIcon()
+        icon4.addPixmap(QtGui.QPixmap("edit-2-fill.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton_7.setIcon(icon4)
+        self.pushButton_7.setObjectName("pushButton_7")
+        self.verticalLayout_6.addWidget(self.pushButton_7)
+        self.verticalLayout_5.addLayout(self.verticalLayout_6)
+        spacerItem5 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_5.addItem(spacerItem5)
+        spacerItem6 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_5.addItem(spacerItem6)
+        spacerItem7 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_5.addItem(spacerItem7)
+        spacerItem8 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_5.addItem(spacerItem8)
+        spacerItem9 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_5.addItem(spacerItem9)
+        spacerItem10 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_5.addItem(spacerItem10)
+        spacerItem11 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_5.addItem(spacerItem11)
+        spacerItem12 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_5.addItem(spacerItem12)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setStyleSheet("QPushButton {\n"
+"    color: rgb(141, 110, 58);\n"
+"    border: none;\n"
+"    font: 10pt \"Arial Rounded MT Bold\";\n"
+"height: 25px;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"    background-color: rgb(149, 117, 61);\n"
+"    color: rgb(255, 255, 255);\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    border-style: inset;\n"
+"    background-color: rgb(149, 117, 61);\n"
+"    color: rgb(255, 255, 255);\n"
+"}")
+        icon5 = QtGui.QIcon()
+        icon5.addPixmap(QtGui.QPixmap("logout-box-r-line.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton.setIcon(icon5)
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout_5.addWidget(self.pushButton)
+        spacerItem13 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_5.addItem(spacerItem13)
+        self.horizontalLayout.addLayout(self.verticalLayout_5)
+        spacerItem14 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.horizontalLayout.addItem(spacerItem14)
+        spacerItem15 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.horizontalLayout.addItem(spacerItem15)
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        self.verticalLayout_7 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_7.setObjectName("verticalLayout_7")
+        spacerItem16 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_7.addItem(spacerItem16)
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setStyleSheet("font: 10pt \"Arial Rounded MT Bold\";\n"
+"color: rgb(141, 110, 58);")
+        self.label_3.setObjectName("label_3")
+        self.verticalLayout_7.addWidget(self.label_3)
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setStyleSheet("QLineEdit {\n"
+"border-radius: 6px;\n"
+"border: 1px solid #d3c393;\n"
+"background-color: #white;\n"
+"color:  #95753d;\n"
+"height: 20px;\n"
+"}")
+        self.lineEdit.setObjectName("lineEdit")
+        self.verticalLayout_7.addWidget(self.lineEdit)
+        spacerItem17 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.verticalLayout_7.addItem(spacerItem17)
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setStyleSheet("font: 10pt \"Arial Rounded MT Bold\";\n"
+"color: rgb(141, 110, 58);")
+        self.label_4.setObjectName("label_4")
+        self.verticalLayout_7.addWidget(self.label_4)
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setStyleSheet("QLineEdit {\n"
+"border-radius: 6px;\n"
+"border: 1px solid #d3c393;\n"
+"background-color: #white;\n"
+"color:  #95753d;\n"
+"height: 20px;\n"
+"}")
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.verticalLayout_7.addWidget(self.lineEdit_2)
+        spacerItem18 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.verticalLayout_7.addItem(spacerItem18)
+        self.label_5 = QtWidgets.QLabel(self.centralwidget)
+        self.label_5.setStyleSheet("font: 10pt \"Arial Rounded MT Bold\";\n"
+"color: rgb(141, 110, 58);")
+        self.label_5.setObjectName("label_5")
+        self.verticalLayout_7.addWidget(self.label_5)
+        self.lineEdit_3 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_3.setStyleSheet("QLineEdit {\n"
+"border-radius: 6px;\n"
+"border: 1px solid #d3c393;\n"
+"background-color: #white;\n"
+"color:  #95753d;\n"
+"height: 20px;\n"
+"}")
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.verticalLayout_7.addWidget(self.lineEdit_3)
+        self.label_6 = QtWidgets.QLabel(self.centralwidget)
+        self.label_6.setStyleSheet("color: rgb(141, 110, 58);")
+        self.label_6.setObjectName("label_6")
+        self.verticalLayout_7.addWidget(self.label_6)
+        spacerItem19 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.verticalLayout_7.addItem(spacerItem19)
+        self.label_7 = QtWidgets.QLabel(self.centralwidget)
+        self.label_7.setStyleSheet("font: 10pt \"Arial Rounded MT Bold\";\n"
+"color: rgb(141, 110, 58);")
+        self.label_7.setObjectName("label_7")
+        self.verticalLayout_7.addWidget(self.label_7)
+        self.lineEdit_4 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_4.setStyleSheet("QLineEdit {\n"
+"border-radius: 6px;\n"
+"border: 1px solid #d3c393;\n"
+"background-color: #white;\n"
+"color:  #95753d;\n"
+"height: 20px;\n"
+"}")
+        self.lineEdit_4.setObjectName("lineEdit_4")
+        self.verticalLayout_7.addWidget(self.lineEdit_4)
+        self.label_8 = QtWidgets.QLabel(self.centralwidget)
+        self.label_8.setStyleSheet("color: rgb(141, 110, 58);")
+        self.label_8.setObjectName("label_8")
+        self.verticalLayout_7.addWidget(self.label_8)
+        spacerItem20 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.verticalLayout_7.addItem(spacerItem20)
+        self.label_9 = QtWidgets.QLabel(self.centralwidget)
+        self.label_9.setStyleSheet("font: 10pt \"Arial Rounded MT Bold\";\n"
+"color: rgb(141, 110, 58);")
+        self.label_9.setObjectName("label_9")
+        self.verticalLayout_7.addWidget(self.label_9)
+        self.lineEdit_5 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_5.setStyleSheet("QLineEdit {\n"
+"border-radius: 6px;\n"
+"border: 1px solid #d3c393;\n"
+"background-color: #white;\n"
+"color:  #95753d;\n"
+"height: 20px;\n"
+"}")
+        self.lineEdit_5.setObjectName("lineEdit_5")
+        self.verticalLayout_7.addWidget(self.lineEdit_5)
+        spacerItem21 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.verticalLayout_7.addItem(spacerItem21)
+        self.horizontalLayout_4 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_4.setObjectName("horizontalLayout_4")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setStyleSheet("QPushButton {\n"
+"border-radius: 6px;\n"
+"border: 2px solid #95753d;\n"
+"background-color: #white;\n"
+"color: #95753d;\n"
+"height: 25px;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"    background-color: rgb(116, 91, 47);\n"
+"    color: rgb(255, 255, 255);\n"
+"    border: 3px solid  rgb(116, 91, 47);\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    border-style: inset;\n"
+"    background-color: rgb(103, 80, 41);\n"
+"    color: rgb(255, 255, 255);\n"
+"    border: 3px solid  rgb(103, 80, 41);\n"
+"}")
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.horizontalLayout_4.addWidget(self.pushButton_2)
+        spacerItem22 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_4.addItem(spacerItem22)
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setStyleSheet("QPushButton {\n"
+"border-radius: 6px;\n"
+"border: 3px solid #95753d;\n"
+"background-color: #95753d;\n"
+"color: white;\n"
+"height: 25px;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"    background-color: rgb(116, 91, 47);\n"
+"    color: rgb(255, 255, 255);\n"
+"    border: 3px solid  rgb(116, 91, 47);\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    border-style: inset;\n"
+"    background-color: rgb(103, 80, 41);\n"
+"    color: rgb(255, 255, 255);\n"
+"    border: 3px solid  rgb(103, 80, 41);\n"
+"}")
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.horizontalLayout_4.addWidget(self.pushButton_3)
+        self.verticalLayout_7.addLayout(self.horizontalLayout_4)
+        spacerItem23 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_7.addItem(spacerItem23)
+        self.horizontalLayout_3.addLayout(self.verticalLayout_7)
+        self.horizontalLayout.addLayout(self.horizontalLayout_3)
+        spacerItem24 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.horizontalLayout.addItem(spacerItem24)
+        spacerItem25 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.horizontalLayout.addItem(spacerItem25)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label_2.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:24pt; color:#ffffff;\">....</span><span style=\" font-size:24pt;\">Notarius</span><span style=\" font-size:24pt; color:#ffffff;\">....</span></p></body></html>"))
+        self.pushButton_4.setText(_translate("MainWindow", "Tabla"))
+        self.pushButton_6.setText(_translate("MainWindow", "Historial"))
+        self.pushButton_9.setText(_translate("MainWindow", "Importar Excel"))
+        self.pushButton_8.setText(_translate("MainWindow", "Agregar Registro"))
+        self.pushButton_7.setText(_translate("MainWindow", "Modificar Registro"))
+        self.pushButton.setText(_translate("MainWindow", "Cerrar Sesión"))
+        self.label_3.setText(_translate("MainWindow", "ID"))
+        self.label_4.setText(_translate("MainWindow", "Input"))
+        self.label_5.setText(_translate("MainWindow", "Input"))
+        self.label_6.setText(_translate("MainWindow", "Your password is between 4 and 12 characters"))
+        self.label_7.setText(_translate("MainWindow", "Input Text Label"))
+        self.label_8.setText(_translate("MainWindow", "Error message informing me of a problem"))
+        self.label_9.setText(_translate("MainWindow", "Input"))
+        self.pushButton_2.setText(_translate("MainWindow", "     Cancel     "))
+        self.pushButton_3.setText(_translate("MainWindow", "     Guardar     "))
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
