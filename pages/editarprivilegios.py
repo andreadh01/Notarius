d8f0f508ef068d6bddac206b1ebcc41bea74c92d
Jared <jaredbarojas90@gmail.com>
2023-02-25 20:28:53 -0700

    checkboxes recuerdan seleccion aunque cambien de tabla/accion

--- pages/editarprivilegios.py
+++ pages/editarprivilegios.py
@@ -2,6 +2,7 @@ from PyQt5 import uic,QtWidgets
 import os
 
 from bdConexion import obtener_conexion
+from functools import partial
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 Form, Base = uic.loadUiType(os.path.join(current_dir,("../ui/editar-privilegios.ui")))
@@ -9,6 +10,9 @@ Form, Base = uic.loadUiType(os.path.join(current_dir,("../ui/editar-privilegios.
 
 class EditarPrivilegios(Base, Form):
 	cols = []
+	diccionario_permisos = {'Agregar':{},
+							'Modificar':{},
+							'Ver':{}}
 	def __init__(self, parent=None):
 		super(self.__class__, self).__init__(parent)
 		self.setupUi(self)
@@ -18,6 +22,7 @@ class EditarPrivilegios(Base, Form):
 		self.setupColumns(self)
 		# cada que se actualice el combobox de tablas, se actualizan los checkbox de las columnas
 		self.tablaslist.currentTextChanged.connect(self.setupColumns)
+		self.accioneslist.currentTextChanged.connect(self.resetCheckboxes)
   
 	# en esta funcion se van a cargar los usuarios de la base de datos al combobox de usuarios
 	def setupUsers(self, Form):
@@ -68,13 +73,32 @@ class EditarPrivilegios(Base, Form):
 			attr.setText(col)
 			self.gridLayout.addWidget(attr, i+1, 3, 1, 1)
 			self.cols.append(attr)
-   
+		self.connectCheckboxes(self)
+		self.resetCheckboxes(self)
+
 	def resetCombobox(self, Form):
 		for obj in self.cols:
 			self.gridLayout.removeWidget(obj)
 			obj.deleteLater()
 			del obj
 		self.cols = []
+
+	def connectCheckboxes(self, Form):
+		for obj in self.cols:
+			obj.stateChanged.connect(partial(self.guardar_opcion,obj))
+
+	def resetCheckboxes(self, Form):
+		for obj in self.cols:
+			if self.tablaslist.currentText() not in self.diccionario_permisos[self.accioneslist.currentText()]:
+				self.diccionario_permisos[self.accioneslist.currentText()][self.tablaslist.currentText()] = {}
+			if obj.text() in self.diccionario_permisos[self.accioneslist.currentText()][self.tablaslist.currentText()]:
+				obj.setChecked(self.diccionario_permisos[self.accioneslist.currentText()][self.tablaslist.currentText()][obj.text()])
+			else:
+				obj.setChecked(False)
+
+	def guardar_opcion(self, obj):
+		self.diccionario_permisos[self.accioneslist.currentText()][self.tablaslist.currentText()][obj.text()] = obj.isChecked()
+
 
 	# al seleccionar guardar se llevara a cabo el comando en la base de datos
 	#	ejemplo: GRANT SELECT (col1), INSERT (col1,col2) ON mydb.mytbl TO 'someuser'@'somehost';
