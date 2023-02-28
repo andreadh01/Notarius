19f0c759fc924dfe104d4d70c4f86977b9ac27af
Andrea D <andreadh01@gmail.com>
2023-02-27 17:43:55 -0700

    Arreglar bug donde no se encontraba el parent al inicializar pantalla de ver tabla

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from PyQt5.QtWidgets import QDialog, QApplication, QWidget
 from PyQt5.QtGui import QPixmap
 from bdConexion import obtener_conexion
 from functools import partial
+from pages.vertabla import VerTabla
 
 class WelcomeScreen(QDialog):
     def __init__(self):
@@ -12,17 +13,6 @@ class WelcomeScreen(QDialog):
         loadUi("ui/login.ui",self)
         self.passwordfield.setEchoMode(QtWidgets.QLineEdit.Password)
         self.login.clicked.connect(self.loginfunction)
-        #self.create.clicked.connect(self.gotocreate)
-
-    # def gototabla(self):
-    #     login = Tabla()
-    #     widget.addWidget(login)
-    #     widget.setCurrentIndex(widget.currentIndex()+1)
-
-    # def gotocreate(self):
-    #     create = CreateAccScreen()
-    #     widget.addWidget(create)
-    #     widget.setCurrentIndex(widget.currentIndex() + 1)
         
     def loginfunction(self):
         user = self.user.text()
@@ -47,65 +37,16 @@ class WelcomeScreen(QDialog):
             if result_pass is not None and result_pass[0] == password:
                 print("Successfully logged in.")
                 dashboard = Dashboard()
+                vertabla = VerTabla(widget)
                 widget.addWidget(dashboard)
+                widget.addWidget(vertabla)
                 widget.setCurrentIndex(widget.currentIndex()+1)
+                
                 self.error.setText("")
             else:
                 self.error.setText("Usuario o contraseña incorrectos. Consulta al administrador.")
                 self.passwordfield.setText('')
                 self.user.setText('')
-
-
-
-class Tabla(QDialog):
-    def __init__(self):
-        super(Tabla, self).__init__()
-        loadUi("interfaces/ver-tabla.ui",self)
-        self.agregar.clicked.connect(self.gotocreate)
-        self.create.clicked.connect(self.gotocreate)
-        self.create.clicked.connect(self.gotocreate)
-        self.create.clicked.connect(self.gotocreate)
-        
-    def gotocreate(self):
-        create = CreateAccScreen()
-        widget.addWidget(create)
-        widget.setCurrentIndex(widget.currentIndex() + 1)
-        
-
-class CreateAccScreen(QDialog):
-    def __init__(self):
-        super(CreateAccScreen, self).__init__()
-        loadUi("interfaces/createacc.ui",self)
-
-#     def signupfunction(self):
-#         user = self.emailfield.text()
-#         password = self.passwordfield.text()
-#         confirmpassword = self.confirmpasswordfield.text()
-
-#         if len(user)==0 or len(password)==0 or len(confirmpassword)==0:
-#             self.error.setText("Please fill in all inputs.")
-
-#         elif password!=confirmpassword:
-#             self.error.setText("Passwords do not match.")
-#         else:
-#             conn = sqlite3.connect("shop_data.db")
-#             cur = conn.cursor()
-
-#             user_info = [user, password]
-#             cur.execute('INSERT INTO login_info (username, password) VALUES (?,?)', user_info)
-
-#             conn.commit()
-#             conn.close()
-
-#             fillprofile = FillProfileScreen()
-#             widget.addWidget(fillprofile)
-#             widget.setCurrentIndex(widget.currentIndex()+1)
-
-# class FillProfileScreen(QDialog):
-#     def __init__(self):
-#         super(FillProfileScreen, self).__init__()
-#         loadUi("fillprofile.ui",self)
-#         self.image.setPixmap(QPixmap('placeholder.png'))
 
 Form, Base = uic.loadUiType("ui/dashboard.ui")
 
@@ -113,7 +54,7 @@ class Dashboard(Base, Form):
 	def __init__(self, parent=None):
 		super(self.__class__, self).__init__(parent)
 		self.setupUi(self)
-		buttons = (self.botonTabla,self.botonAgregar, self.botonEditarPrivilegios, self.botonRegistrar)
+		buttons = (self.botonTabla,self.botonAgregar, self.botonEditarPrivilegios, self.botonRegistrar, self.botonEditarReg)
 		for i, button in enumerate(buttons):
 			button.clicked.connect(partial(self.stackedWidget.setCurrentIndex, i))
 
