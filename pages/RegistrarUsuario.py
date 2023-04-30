5e66828f708977586ae289216ec3c03e9390a077
estefy <103406845+estefy22@users.noreply.github.com>
2023-04-30 01:12:17 -0700

    Letras grandes pt1

--- pages/RegistrarUsuario.py
+++ pages/RegistrarUsuario.py
@@ -105,7 +105,7 @@ class RegistrarUsuario(Form, Base):
             setattr(self, name_escritura, QtWidgets.QCheckBox(Form))
             checkbox_ver = getattr(self,name_ver)
             checkbox_ver.setStyleSheet("\n"
-            "font: 75 11pt;\n"
+            "font: 75 14pt;\n"
             "color: white;")
             checkbox_ver.setObjectName(name_ver)
             
@@ -113,7 +113,7 @@ class RegistrarUsuario(Form, Base):
             checkbox_escritura.setText(col)
             checkbox_escritura = getattr(self,name_escritura)
             checkbox_escritura.setStyleSheet("\n"
-            "font: 75 11pt;\n"
+            "font: 75 14pt;\n"
             "color: white;")
             checkbox_escritura.setObjectName(name_escritura)
             checkbox_escritura.setText(col)
